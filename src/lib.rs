f255e3e717801be743396ff89aab8489dad4e6fb
Luca Barbato <lu_zero@gentoo.org>
2021-11-16 00:48:34 +0100

    Move the Endpoint abstraction to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,127 @@
+use std::collections::HashMap;
+
+use anyhow::{bail, Context, Result};
+use tokio::net::*;
+
+impl EndPoint {
+    pub fn setup_udp(&self, localaddr: SocketAddr) -> Result<UdpSocket> {
+        use socket2::*;
+
+        let udp_ip = self.addr.ip();
+        let is_multicast = udp_ip.is_multicast();
+        let sockaddr = SockAddr::from(localaddr);
+
+        let udp = match udp_ip {
+            IpAddr::V4(ref addr) => {
+                let udp = Socket::new(Domain::ipv4(), Type::dgram(), Some(Protocol::udp()))?;
+                udp.set_reuse_address(true)?;
+                udp.set_reuse_port(true)?;
+                if let Some(udp_buffer) = self.buffer {
+                    udp.set_send_buffer_size(udp_buffer)?;
+                    udp.set_recv_buffer_size(udp_buffer)?;
+                }
+                udp.bind(&sockaddr)?;
+                if is_multicast {
+                    let mcast_if = match self.multicast_interface_address {
+                        Some(ref mcast_if) => mcast_if,
+                        None => &Ipv4Addr::UNSPECIFIED,
+                    };
+                    udp.join_multicast_v4(addr, mcast_if)
+                        .context("cannot join group")?;
+
+                    if let Some(ttl) = self.multicast_ttl {
+                        udp.set_multicast_ttl_v4(ttl)?;
+                    }
+                }
+                udp
+            }
+            IpAddr::V6(ref addr) => {
+                let udp = Socket::new(Domain::ipv6(), Type::dgram(), Some(Protocol::udp()))?;
+                udp.set_reuse_address(true)?;
+                udp.set_reuse_port(true)?;
+                if let Some(udp_buffer) = self.buffer {
+                    udp.set_send_buffer_size(udp_buffer)?;
+                    udp.set_recv_buffer_size(udp_buffer)?;
+                }
+                udp.bind(&sockaddr)?;
+                if is_multicast {
+                    let mcast_idx = match self.multicast_interface_index {
+                        Some(mcast_idx) => mcast_idx,
+                        None => 0,
+                    };
+
+                    udp.join_multicast_v6(addr, mcast_idx)?;
+
+                    if let Some(hops) = self.multicast_hops {
+                        udp.set_multicast_hops_v6(hops)?;
+                    }
+                }
+                udp
+            }
+        };
+
+        let udp = UdpSocket::from_std(udp.into_udp_socket())?;
+
+        Ok(udp)
+    }
+}
+
+use url::Url;
+
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+pub fn to_endpoint(s: &str) -> Result<EndPoint> {
+    let u = Url::parse(s)?;
+
+    if u.scheme() != "udp" {
+        bail!("Only udp is supported {:?}", u);
+    }
+
+    let socks = u.socket_addrs(|| Some(5555))?;
+
+    let addr = *socks
+        .first()
+        .ok_or(anyhow::anyhow!("No address for name {}", s))?;
+
+    let query = u.query_pairs().collect::<HashMap<_, _>>();
+
+    let mut multicast_interface_address = None;
+    let mut multicast_interface_index = None;
+
+    if let Some(s) = query.get("multicast") {
+        if addr.is_ipv4() {
+            multicast_interface_address = Some(s.parse()?);
+        } else {
+            multicast_interface_index = Some(s.parse()?);
+        }
+    }
+
+    let multicast_ttl = query.get("multicast_ttl").map(|m| m.parse()).transpose()?;
+    let multicast_hops = query.get("multicast_hops").map(|m| m.parse()).transpose()?;
+    let buffer = query.get("buffer").map(|m| m.parse()).transpose()?;
+
+    Ok(EndPoint {
+        multicast_interface_address,
+        multicast_interface_index,
+        multicast_ttl,
+        multicast_hops,
+        buffer,
+        addr,
+    })
+}
+
+#[derive(Debug)]
+pub struct EndPoint {
+    /// UDP multicast interface address (IPv4 only)
+    pub multicast_interface_address: Option<Ipv4Addr>,
+    /// UDP multicast interface index (IPv6 only)
+    pub multicast_interface_index: Option<u32>,
+    /// UDP address in `ip:port` format
+    pub addr: SocketAddr,
+    /// IPv4 Multicast TTL
+    pub multicast_ttl: Option<u32>,
+    /// IPv6 Multicast Hops
+    pub multicast_hops: Option<u32>,
+    /// UDP OS buffer size
+    pub buffer: Option<usize>,
+}
